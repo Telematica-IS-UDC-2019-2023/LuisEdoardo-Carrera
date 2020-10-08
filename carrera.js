72f9ce141c41b2f8813fb1f94ea912f735492534
Luis Leyva <lmorales6@ucol.mx>
2020-10-08 14:56:55 -0500

    Finalizado

---
+++ carrera.js
@@ -0,0 +1,35 @@
+class Dado {
+    lanzarDado() {
+        let cara = Math.ceil(Math.random() * 6);
+        if (cara == 1) {
+            return 3
+        } else if (cara == 2 || cara == 3) {
+            return 1
+        } else if (cara == 4 || cara == 5 || cara == 6) {
+            return 2
+        }
+    }
+}
+class Corredor {
+    constructor(name) {
+        this.name = name
+        this.posicion = 0
+        this.dado = new Dado
+    }
+    correr() {
+        return this.posicion += this.dado.lanzarDado();
+    }
+}
+let R = new Corredor('Rojo');
+let A = new Corredor('Azul');
+while (R.posicion < 100 && A.posicion < 100) {
+    R.correr();
+    A.correr();
+}
+if (R.posicion >= 100 && A.posicion >= 100) {
+    console.log('Ambos empataron');
+} else if (R.posicion >= 100) {
+    console.log(`${R.name} ganó`);
+} else if (A.posicion >= 100) {
+    console.log(`${A.name} ganó`);
+}
